2f21ea0632166175469205895e964abdcb84cd97
luis.Paulo <luispaulo.ads8@gmail.com>
2024-11-15 20:18:29 -0300

    configurando projeto com biblioteca e mapeamento e EF

---
+++ ClienteProjeto.Application/Mappings/DTOMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ClienteProjeto.Application.DTOs;
+using ClienteProjeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteProjeto.Application.Mappings;
+
+public class DTOMappingProfile : Profile
+{
+    public DTOMappingProfile()
+    {
+        CreateMap<ProdutoServico, ProdutoServicoDTO>().ReverseMap();
+    }
+}
