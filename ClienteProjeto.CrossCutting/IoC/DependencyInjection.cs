d0545814925e0b37c11fe14753bcd34cd67be16f
luis.Paulo <luispaulo.ads8@gmail.com>
2024-11-18 18:42:55 -0300

    configurando classe que faz conexão com banco e suas dependencias do projeto e atualizar no projeto devido atualização dos pacotes do Nuget

---
+++ ClienteProjeto.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using ClienteProjeto.Domain.Interfaces;
+using ClienteProjeto.Infrastructure.Context;
+using ClienteProjeto.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClienteProjeto.CrossCutting.IoC;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureAPI(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseMySql(configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 29))));
+
+        services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+
+        return services;
+    }
+}
