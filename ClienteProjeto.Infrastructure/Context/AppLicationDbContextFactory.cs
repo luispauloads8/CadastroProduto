7df660b5b744da46390ebe037148bb56e1ef95fc
luis.Paulo <luispaulo.ads8@gmail.com>
2024-11-18 18:40:06 -0300

    configurando class de context para acesso ao banco de dados

---
+++ ClienteProjeto.Infrastructure/Context/AppLicationDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace ClienteProjeto.Infrastructure.Context;
+
+internal class AppLicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Ajuste o caminho para o diretório onde o appsettings.json está localizado
+        string basePath = Path.Combine(Directory.GetCurrentDirectory(), "../ClienteProjeto.API");
+
+        // Carrega as configurações do appsettings.json
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Configura o DbContextOptionsBuilder com o MySQL
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 38)));
+
+        return new ApplicationDbContext(builder.Options);
+    }
+}
