65c4fc2a04218ae1cb331d2ec50bcbeb9e374a3b
luis.Paulo <luispaulo.ads8@gmail.com>
2025-01-06 22:30:10 -0300

    ajustando component nav

--- AngularApp/src/app/nav/nav.component.ts
+++ AngularApp/src/app/nav/nav.component.ts
@@ -1,14 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterModule, BsDropdownModule],
+  imports: [RouterModule, BsDropdownModule, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 
+  constructor(private route: Router){}
+
+  showMenu(): boolean{
+    return this.route.url != '/user/login';
+  }
 }
