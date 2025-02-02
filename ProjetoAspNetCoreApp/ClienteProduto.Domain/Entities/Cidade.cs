793e10ae555f869b56731329ea9e9396255de650
luis.Paulo <luispaulo.ads8@gmail.com>
2025-02-02 00:06:36 -0300

    ajustando a class cidade e criando enum EStado

--- ProjetoAspNetCoreApp/ClienteProduto.Domain/Entities/Cidade.cs
+++ ProjetoAspNetCoreApp/ClienteProduto.Domain/Entities/Cidade.cs
@@ -9,6 +9,7 @@ public class Cidade : Entity
     }
 
     public string Descricao { get; set; }
+    public EnumEstado Estado { get; set;}
     public ICollection<Cliente> ClientesEndereco { get; set; }
     public ICollection<Empresa> Empresas { get; set; }
     public ICollection<Fornecedor> Fornecedores { get; set; }
