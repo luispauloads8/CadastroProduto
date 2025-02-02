741ca43a09eeadc56e2614b080da1a5673d8e18c
luis.Paulo <luispaulo.ads8@gmail.com>
2025-02-02 00:05:45 -0300

    migration de adicionar enum estado

---
+++ ProjetoAspNetCoreApp/ClienteProjeto.Infrastructure/Migrations/20250201190903_adicionandoEnumEstado.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClienteProjeto.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicionandoEnumEstado : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Estado",
+                table: "Cidades",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Cidades",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Estado",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Cidades",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Estado",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Cidades",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Estado",
+                value: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Cidades");
+        }
+    }
+}
