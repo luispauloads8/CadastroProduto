78cb15bfca31e4270aa90df6e357ddacebd3e105
luis.Paulo <luispaulo.ads8@gmail.com>
2025-02-10 19:01:52 -0300

    aplicando migrations para ajuste e implementação

---
+++ ProjetoAspNetCoreApp/ClienteProjeto.Infrastructure/Migrations/20250210194610_AjustandoReferenciaCidade.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClienteProjeto.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjustandoReferenciaCidade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CidadeId",
+                table: "Empresas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CidadeId",
+                table: "Clientes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CidadeId",
+                table: "Empresas");
+
+            migrationBuilder.DropColumn(
+                name: "CidadeId",
+                table: "Clientes");
+        }
+    }
+}
---
+++ ProjetoAspNetCoreApp/ClienteProjeto.Infrastructure/Migrations/20250210202157_removeIdCidade.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClienteProjeto.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeIdCidade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empresas_Cidades_CidadeEmpresaId",
+                table: "Empresas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Empresas_CidadeEmpresaId",
+                table: "Empresas");
+
+            migrationBuilder.DropColumn(
+                name: "CidadeEmpresaId",
+                table: "Empresas");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresas_CidadeId",
+                table: "Empresas",
+                column: "CidadeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empresas_Cidades_CidadeId",
+                table: "Empresas",
+                column: "CidadeId",
+                principalTable: "Cidades",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empresas_Cidades_CidadeId",
+                table: "Empresas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Empresas_CidadeId",
+                table: "Empresas");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CidadeEmpresaId",
+                table: "Empresas",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empresas_CidadeEmpresaId",
+                table: "Empresas",
+                column: "CidadeEmpresaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empresas_Cidades_CidadeEmpresaId",
+                table: "Empresas",
+                column: "CidadeEmpresaId",
+                principalTable: "Cidades",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
