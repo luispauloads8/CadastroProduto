c08d62203a1365f33cd43ef7d44d41e6f524413e
luis.Paulo <luispaulo.ads8@gmail.com>
2025-06-26 01:20:11 -0300

    criando DTOMapping

---
+++ ProjetoAspNetCoreApp/ClienteProjeto.Application/Mappings/DTOMappingProfileFiltro.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using ClienteProjeto.Application.DTOs.Filtro;
+using ClienteProjeto.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteProjeto.Application.Mappings
+{
+    public class DTOMappingProfileFiltro : Profile
+    {
+        public DTOMappingProfileFiltro()
+        {
+            CreateMap<DtoFiltroLancamento, Lancamento>()
+            .ForMember(dest => dest.EmpresaId, opt => opt.MapFrom(src => src.EmpresaId))
+            .ForMember(dest => dest.ContaContabilId, opt => opt.MapFrom(src => src.ContaContabilId))
+            .ForMember(dest => dest.ClienteId, opt => opt.MapFrom(src => src.ClienteId))
+            .ForMember(dest => dest.ProdutoServicoId, opt => opt.MapFrom(src => src.ProdutoServicoId))
+            .ForMember(dest => dest.DataLancamento, opt => opt.MapFrom(src => src.VencimentoInicio)).ReverseMap(); // ou ajuste correto
+                                                                                                     // Adicione os demais campos se necessário
+        }
+    }
+}
