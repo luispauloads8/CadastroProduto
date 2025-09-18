65f2d7101f0583fa8ccd7b07bab373166e82e929
luis.Paulo <luispaulo.ads8@gmail.com>
2025-09-18 16:01:38 -0300

    cadastro de enum

---
+++ ProjetoAspNetCoreApp/ClienteProduto.Domain/Enum/FormaPagamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteProjeto.Domain.Enum
+{
+    public enum FormaDePagamento
+    {
+        Pix = 0,
+        Dinheiro = 1,
+        Cartão = 2,
+    }
+}
